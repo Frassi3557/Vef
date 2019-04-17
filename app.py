f2e864682bd2491d8103380d1a8fcf9294a19a6e
Frassi3557 <freysi_freys@live.com>
2019-04-17 20:55:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import os
-import psycopg2 
-import sqlite3
-import json
-import urllib.request
 from flask import Flask, render_template, request, session, redirect, url_for, escape
 from jinja2 import Template
-#from . import app
-#from wtforms import EmailPasswordForm
 
 app = Flask(__name__)
 
