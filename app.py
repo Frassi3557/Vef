121bfb8f82563e1638c5fcccbfe857ee8a04178f
Frassi3557 <freysi_freys@live.com>
2019-04-17 20:52:11 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import psycopg2 
+import sqlite3
+import json
+import urllib.request
+from flask import Flask, render_template, request, session, redirect, url_for, escape
+from jinja2 import Template
+#from . import app
+#from wtforms import EmailPasswordForm
+
+app = Flask(__name__)
+
+#DATABASE_URL = os.environ['DATABASE_URL']
+
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+#app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def student():
+   return "render_template('student.tpl')"
+
+if __name__ == '__main__':
+    app.run()
+    #app.run(debug=True, use_reloader=True) 
