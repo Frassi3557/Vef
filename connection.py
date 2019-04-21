a9f04c8c30ef98a9977e217e84bc0b30d4802565
Frassi3557 <freysi_freys@live.com>
2019-04-21 21:48:47 +0000

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,3 +1,29 @@
+#from config import config
+#import psycopg2
+
+#@app.route('/delete')
+#def delete():
+#    delete = "DELETE FROM users"
+#    conn = None
+#    try:
+#        # read the connection parameters
+#        params = config()
+#        # connect to the PostgreSQL server
+#        conn = psycopg2.connect(**params)
+#        cur = conn.cursor()
+#        # create table one by one
+#        cur.execute(delete)
+#        # close communication with the PostgreSQL database server
+#        cur.close()
+#        # commit the changes
+#        conn.commit()
+#    except (Exception, psycopg2.DatabaseError) as error:
+#        print(error)
+#    finally:
+#        if conn is not None:
+#            conn.close()
+#
+#    
 #def create_tables():
 #    """ create tables in the PostgreSQL database"""
 #    commands = (
